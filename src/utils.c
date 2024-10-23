e928ad2f02013d0a47e053a9f7b32009315d9e75
Tiago Sodre Pereira <tiagosodrepereira@gmail.com>
2024-10-23 16:52:21 +0100

    small change

--- src/utils.c
+++ src/utils.c
@@ -157,27 +157,3 @@ char	*ft_strcjoin(char const *s1, char c)
 	join[i] = '\0';
 	return (join);
 }
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char			*join;
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	size;
-
-	i = 0;
-	j = 0;
-	if (!s1 || !s2)
-		return (0);
-	size = ft_strlen(s1) + ft_strlen(s2);
-	join = (char *)malloc(sizeof(char) * (size + 1));
-	if (!join)
-		return (0);
-	while (s1[j])
-		join[i++] = s1[j++];
-	j = 0;
-	while (s2[j])
-		join[i++] = s2[j++];
-	join[i] = '\0';
-	return (join);
-}
