13db902bd749bc1865894391eb10cf1e4d197cb3
davioliv <davi.mlp@gmail.com>
2024-10-24 13:28:56 +0100

    now it uses builtins

--- src/check_builtins.c
+++ src/check_builtins.c
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:55:04 by tsodre-p          #+#    #+#             */
-/*   Updated: 2024/10/16 13:39:41 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2024/10/17 15:38:28 by davioliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,19 @@
 int	do_builtin(t_minishell *ms, char **query)
 {
 	if (!(ft_strncmp(query[0], "echo", 4)))
+	{
 		ft_echo(ms, query);
+		return (1);
+	}
 	else if (!(ft_strncmp(query[0], "env", 3)))
+	{
 		ft_env(ms, query);
+		return (1);
+	}
 	else if (!(ft_strncmp(query[0], "cd", 2)))
+	{
 		ft_cd(ms ,query);
+		return (1);
+	}
 	return (0);
 }
